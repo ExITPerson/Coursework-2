df7635e8eef63432ebeff815d5339633ac72ebb2
Ex_person <baranovs11@yandex.ru>
2024-12-23 17:08:12 +0300

    test func save file

--- src/file_saver.py
+++ src/file_saver.py
@@ -44,13 +44,14 @@ class JsonSaver(AbstractSaveFile):
 
 class CSVSaver(AbstractSaveFile):
 
+    path = "data"
     file_name = None
 
     @classmethod
     def save_data(cls, data, name):
         cls.file_name = name
-        if os.path.exists(f"data/{cls.file_name}.csv"):
-            csv_file = f"data/{cls.file_name}.csv"
+        if os.path.exists(f"{cls.path}/{cls.file_name}.csv"):
+            csv_file = f"{cls.path}/{cls.file_name}.csv"
             try:
                 df = pd.read_csv(csv_file)
                 data_file = df.to_dict("records")
@@ -59,21 +60,21 @@ class CSVSaver(AbstractSaveFile):
                 new_data.extend(data)
 
                 df = pd.DataFrame(new_data)
-                df.to_csv(f"data/{cls.file_name}.csv", index=False, encoding="utf-8")
+                df.to_csv(f"{cls.path}/{cls.file_name}.csv", index=False, encoding="utf-8")
 
             except pandas.errors.EmptyDataError:
                 df = pd.DataFrame(data)
-                df.to_csv(f"data/{cls.file_name}.csv", index=False, encoding="utf-8")
+                df.to_csv(f"{cls.path}/{cls.file_name}.csv", index=False, encoding="utf-8")
         else:
             df = pd.DataFrame(data)
-            df.to_csv(f"data/{cls.file_name}.csv", index=False, encoding="utf-8")
+            df.to_csv(f"{cls.path}/{cls.file_name}.csv", index=False, encoding="utf-8")
 
 
     @classmethod
     def del_data(cls, data, name):
         cls.file_name = name
-        if os.path.exists(f"data/{cls.file_name}.csv"):
-            csv_file = f"data/{cls.file_name}.csv"
+        if os.path.exists(f"{cls.path}/{cls.file_name}.csv"):
+            csv_file = f"{cls.path}/{cls.file_name}.csv"
             try:
                 df = pd.read_csv(csv_file)
                 data_file = df.to_dict("records")
@@ -81,7 +82,7 @@ class CSVSaver(AbstractSaveFile):
                 new_data = [item for item in data_file if item['id'] not in {x['id'] for x in data}]
 
                 df = pd.DataFrame(new_data)
-                df.to_csv(f"data/{cls.file_name}.csv", index=False, encoding="utf-8")
+                df.to_csv(f"{cls.path}/{cls.file_name}.csv", index=False, encoding="utf-8")
             except pandas.errors.EmptyDataError:
                 print("Файл пустой, удалять нечего!")
         else:
@@ -90,13 +91,14 @@ class CSVSaver(AbstractSaveFile):
 
 class ExcelSaver(AbstractSaveFile):
 
+    path = "data"
     file_name = None
 
     @classmethod
     def save_data(cls, data, name):
         cls.file_name = name
-        if os.path.exists(f"data/{cls.file_name}.xlsx"):
-            csv_file = f"data/{cls.file_name}.xlsx"
+        if os.path.exists(f"{cls.path}/{cls.file_name}.xlsx"):
+            csv_file = f"{cls.path}/{cls.file_name}.xlsx"
             df = pd.read_excel(csv_file)
             data_file = df.to_dict("records")
 
@@ -104,16 +106,16 @@ class ExcelSaver(AbstractSaveFile):
             new_data.extend(data)
 
             df = pd.DataFrame(new_data)
-            df.to_excel(f"data/{cls.file_name}.xlsx", index=False)
+            df.to_excel(f"{cls.path}/{cls.file_name}.xlsx", index=False)
         else:
             df = pd.DataFrame(data)
-            df.to_excel(f"data/{cls.file_name}.xlsx", index=False)
+            df.to_excel(f"{cls.path}/{cls.file_name}.xlsx", index=False)
 
     @classmethod
     def del_data(cls, data, name):
         cls.file_name = name
-        if os.path.exists(f"data/{cls.file_name}.xlsx"):
-            csv_file = f"data/{cls.file_name}.xlsx"
+        if os.path.exists(f"{cls.path}/{cls.file_name}.xlsx"):
+            csv_file = f"{cls.path}/{cls.file_name}.xlsx"
 
             df = pd.read_excel(csv_file)
             data_file = df.to_dict("records")
@@ -121,42 +123,43 @@ class ExcelSaver(AbstractSaveFile):
             new_data = [item for item in data_file if item['id'] not in {x['id'] for x in data}]
 
             df = pd.DataFrame(new_data)
-            df.to_excel(f"data/{cls.file_name}.xlsx", index=False)
+            df.to_excel(f"{cls.path}/{cls.file_name}.xlsx", index=False)
 
         else:
             print("Такого файла не существует")
 
 
 class TXTSaver(AbstractSaveFile):
+    path = "data"
 
     file_name = None
 
     @classmethod
     def save_data(cls, data, name):
         cls.file_name = name
-        if os.path.exists(f"data/{name}.txt"):
-            with open(f"data/{cls.file_name}.txt", "r", encoding="utf-8") as f:
+        if os.path.exists(f"{cls.path}/{name}.txt"):
+            with open(f"{cls.path}/{cls.file_name}.txt", "r", encoding="utf-8") as f:
                 data_file = json.load(f)
 
             data_file = [item for item in data_file if item['id'] not in {x['id'] for x in data}]
             data_file.extend(data)
 
-            with open(f"data/{cls.file_name}.txt", "w", encoding="utf-8") as file:
+            with open(f"{cls.path}/{cls.file_name}.txt", "w", encoding="utf-8") as file:
                 json.dump(data_file, file, indent=4, ensure_ascii=False)
         else:
-            with open(f"data/{cls.file_name}.txt", "w", encoding="utf-8") as f:
+            with open(f"{cls.path}/{cls.file_name}.txt", "w", encoding="utf-8") as f:
                 json.dump(data, f, indent=4, ensure_ascii=False)
 
     @classmethod
     def del_data(cls, data, name):
         cls.file_name = name
-        if os.path.exists(f"data/{cls.file_name}.txt"):
-            with open(f"data/{cls.file_name}.txt", "r", encoding="utf-8") as f:
+        if os.path.exists(f"{cls.path}/{cls.file_name}.txt"):
+            with open(f"{cls.path}/{cls.file_name}.txt", "r", encoding="utf-8") as f:
                 data_file = json.load(f)
 
             data_file = [item for item in data_file if item['id'] not in {x['id'] for x in data}]
 
-            with open(f"data/{cls.file_name}.txt", "w", encoding="utf-8") as file:
+            with open(f"{cls.path}/{cls.file_name}.txt", "w", encoding="utf-8") as file:
                 json.dump(data_file, file, indent=4, ensure_ascii=False)
         else:
             print("Такого файла не существует")
--- tests/test_file_saver.py
+++ tests/test_file_saver.py
@@ -1,9 +1,9 @@
-
+import pandas as pd
 import os
 import json
 
 
-from src.file_saver import JsonSaver
+from src.file_saver import JsonSaver, TXTSaver, ExcelSaver, CSVSaver
 
 
 def test_save_json_new_file(tmp_path):
@@ -55,3 +55,159 @@ def test_del_data_non_file(capsys):
     JsonSaver.del_data(data, "non_file")
     result = capsys.readouterr()
     assert result.out.strip() == "Такого файла не существует"
+
+
+def test_save_json_new_file_txt(tmp_path):
+    data = [{"id": 1, "name": "Test"}]
+    test1 = TXTSaver
+    test1.path = str(tmp_path)
+    test1.save_data(data, "test")
+    file_path = tmp_path / "test.txt"
+
+    assert os.path.exists(file_path)
+
+    with open(file_path, "r", encoding="utf-8") as f:
+        saved_data = json.load(f)
+        assert saved_data == data
+
+
+def test_update_save_data_txt(tmp_path):
+    test2 = TXTSaver
+    test2.path = str(tmp_path)
+    initial_data = [{'id': 1, 'name': 'Test'}]
+    test2.save_data(initial_data, 'test')
+
+    new_data = [{'id': 1, 'name': 'Updated'}, {'id': 2, 'name': 'New'}]
+    test2.save_data(new_data, 'test')
+    file_path = tmp_path / "test.txt"
+
+    with open(file_path, 'r', encoding='utf-8') as f:
+        saved_data = json.load(f)
+        expected_data = [{'id': 1, 'name': 'Updated'}, {'id': 2, 'name': 'New'}]
+        assert saved_data == expected_data
+
+
+def test_del_data_txt(tmp_path):
+    test3 = TXTSaver
+    test3.path = tmp_path
+    data = [{'id': 1, 'name': 'Test'}, {'id': 2, 'name': 'Another'}]
+    test3.save_data(data, "test")
+
+    data_to_del = [{'id': 1, 'name': 'Test'}]
+    test3.del_data(data_to_del, "test")
+    file_path = tmp_path / "test.txt"
+
+    with open(file_path, "r", encoding="utf-8") as f:
+        saved_data = json.load(f)
+        assert saved_data == [{'id': 2, 'name': 'Another'}]
+
+
+def test_del_data_non_file_txt(capsys):
+    data = [{'id': 2, 'name': 'Another'}]
+    TXTSaver.del_data(data, "non_file")
+    result = capsys.readouterr()
+    assert result.out.strip() == "Такого файла не существует"
+
+
+def test_update_save_data_xlsx(tmp_path):
+    test2 = ExcelSaver
+    test2.path = str(tmp_path)
+    initial_data = [{'id': 1, 'name': 'Test'}]
+    test2.save_data(initial_data, 'test')
+
+    new_data = [{'id': 1, 'name': 'Updated'}, {'id': 2, 'name': 'New'}]
+    test2.save_data(new_data, 'test')
+    file_path = tmp_path / "test.xlsx"
+
+    df = pd.read_excel(file_path)
+    saved_data = df.to_dict("records")
+    expected_data = [{'id': 1, 'name': 'Updated'}, {'id': 2, 'name': 'New'}]
+    assert saved_data == expected_data
+
+
+def test_del_data_xlsx(tmp_path):
+    test3 = ExcelSaver
+    test3.path = tmp_path
+    data = [{'id': 1, 'name': 'Test'}, {'id': 2, 'name': 'Another'}]
+    test3.save_data(data, "test")
+
+    data_to_del = [{'id': 1, 'name': 'Test'}]
+    test3.del_data(data_to_del, "test")
+    file_path = tmp_path / "test.xlsx"
+
+    df = pd.read_excel(file_path)
+    saved_data = df.to_dict("records")
+    assert saved_data == [{'id': 2, 'name': 'Another'}]
+
+
+def test_del_data_non_file_xlsx(capsys):
+    data = [{'id': 2, 'name': 'Another'}]
+    ExcelSaver.del_data(data, "non_file")
+    result = capsys.readouterr()
+    assert result.out.strip() == "Такого файла не существует"
+
+
+def test_save_json_new_file_xlsx(tmp_path):
+    data = [{"id": 1, "name": "Test"}]
+    test1 = ExcelSaver
+    test1.path = str(tmp_path)
+    test1.save_data(data, "test")
+    file_path = tmp_path / "test.xlsx"
+
+    assert os.path.exists(file_path)
+
+    df = pd.read_excel(file_path)
+    saved_data = df.to_dict("records")
+    assert saved_data == data
+
+
+def test_update_save_data_csv(tmp_path):
+    test2 = CSVSaver
+    test2.path = str(tmp_path)
+    initial_data = [{'id': 1, 'name': 'Test'}]
+    test2.save_data(initial_data, 'test')
+
+    new_data = [{'id': 1, 'name': 'Updated'}, {'id': 2, 'name': 'New'}]
+    test2.save_data(new_data, 'test')
+    file_path = tmp_path / "test.csv"
+
+    df = pd.read_csv(file_path)
+    saved_data = df.to_dict("records")
+    expected_data = [{'id': 1, 'name': 'Updated'}, {'id': 2, 'name': 'New'}]
+    assert saved_data == expected_data
+
+
+def test_del_data_csv(tmp_path):
+    test3 = CSVSaver
+    test3.path = tmp_path
+    data = [{'id': 1, 'name': 'Test'}, {'id': 2, 'name': 'Another'}]
+    test3.save_data(data, "test")
+
+    data_to_del = [{'id': 1, 'name': 'Test'}]
+    test3.del_data(data_to_del, "test")
+    file_path = tmp_path / "test.csv"
+
+    df = pd.read_csv(file_path)
+    saved_data = df.to_dict("records")
+    assert saved_data == [{'id': 2, 'name': 'Another'}]
+
+
+def test_del_data_non_file_csv(capsys):
+    data = [{'id': 2, 'name': 'Another'}]
+    CSVSaver.del_data(data, "non_file")
+    result = capsys.readouterr()
+    assert result.out.strip() == "Такого файла не существует"
+
+
+def test_save_json_new_file_csv(tmp_path):
+    data = [{"id": 1, "name": "Test"}]
+    test1 = CSVSaver
+    test1.path = str(tmp_path)
+    test1.save_data(data, "test")
+    file_path = tmp_path / "test.csv"
+
+    assert os.path.exists(file_path)
+
+    df = pd.read_csv(file_path)
+    saved_data = df.to_dict("records")
+    assert saved_data == data
